ee0c1a9641f09bde1e1db214c215cef9e5810726
Fernando Saraiva <fernando.saraiva@zup.com.br>
2021-09-17 14:01:54 -0300

    [5.12] adicionado log na criação de autor

--- src/main/kotlin/br/com/zup/autor/CadastroAutorController.kt
+++ src/main/kotlin/br/com/zup/autor/CadastroAutorController.kt
@@ -5,13 +5,17 @@ import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Status
+import org.slf4j.LoggerFactory
 
 @Controller("/api/autores")
 class CadastroAutorController {
 
+    var logger= LoggerFactory.getLogger(this.javaClass)
+
     @Post
     @Status(HttpStatus.CREATED)
     fun cadastra(@Body request: NovoAutorRequest) {
-        println(request)
+        logger.info("Novo AUTOR cadastrado: $request")
     }
+
 }
