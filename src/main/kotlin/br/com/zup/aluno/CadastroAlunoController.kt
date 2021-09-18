544748ac47422104b68324a73cc9cca26dff8186
Fernando Saraiva <fernando.saraiva@zup.com.br>
2021-09-18 16:00:50 -0300

    [5.13] refatoração adicionado retorno NovoAlunoRequest

--- src/main/kotlin/br/com/zup/aluno/CadastroAlunoController.kt
+++ src/main/kotlin/br/com/zup/aluno/CadastroAlunoController.kt
@@ -4,6 +4,7 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Status
 import jakarta.inject.Inject
 
 import org.slf4j.LoggerFactory
@@ -19,7 +20,8 @@ class CadastroAlunoController {
     var logger= LoggerFactory.getLogger(this.javaClass)
 
     @Post
-    fun cadastra(@Body request: NovoAlunoRequest): HttpStatus {
+    @Status(HttpStatus.CREATED)
+    fun cadastra(@Body request: NovoAlunoRequest): NovoAlunoRequest {
         val constraintViolations = validator.validate(request)
 
         if(constraintViolations.isNotEmpty()) {
@@ -27,7 +29,7 @@ class CadastroAlunoController {
         }
 
         logger.info("Novo ALUNO cadastrado: $request")
-        return HttpStatus.OK
+        return request
     }
 
 }
